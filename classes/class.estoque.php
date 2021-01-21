8b8f06d7781d19a37b2bf28e927f95225e4aa911
Humberto Rodrigues <humberto.rodrigues@gmail.com>
2021-01-21 10:57:29 -0300

    shortcode para acompanhamento do estoque

--- classes/class.estoque.php
+++ classes/class.estoque.php
@@ -16,10 +16,72 @@ class estoque {
             $product->save();
         });
         add_action( 'init', function(){
-            if ( ! wp_next_scheduled( 'atualizar_estoque_tiny' ) ) {
-                wp_schedule_event( time(), 'hourly', 'atualizar_estoque_tiny' );
+            add_action('atualizar_estoque_tiny_novo',array($this,'atualizar_estoque_tiny'));
+            if ( ! wp_next_scheduled( 'atualizar_estoque_tiny_novo' ) ) {
+                wp_schedule_event( time(), 'hourly', 'atualizar_estoque_tiny_novo');
             }
         } );
+        add_shortcode('estoque',array($this,"shortcode_estoque"));
+    }
+    public function shortcode_estoque(){
+
+        $query = new WC_Product_Query( array(
+            'limit' => -1,
+            'return' => 'ids',
+        ) );
+        $products = $query->get_products();
+        $dados_estoque = array();
+        foreach ($products as $key => $product) {
+            $estoque = get_post_meta($product,"tiny_estoque",true);
+            if($estoque == ""){
+                continue;
+            }
+            $dados_estoque[$key]['id_produto'] = $product;
+            $dados_estoque[$key]['estoque'] = $estoque;
+            
+            
+        }
+        ?>
+        <h1>Saldo estoques</h1>
+        <table style="margin:20px 0 0 20px">
+        <?php 
+        foreach ($dados_estoque as $key => $dado_estoque): 
+            
+        ?>
+            <?php $total_estoque = 0  ?>
+            
+            <tr>
+                <td style="padding-right:20px"><?php echo get_the_title($dado_estoque['id_produto']) ?></td>
+                <td>
+                    <table>
+                        <?php foreach( $dado_estoque['estoque'] as $deposito): 
+                            if($deposito->deposito->saldo==0){
+                                continue;
+                            }
+                            ?>
+                        <?php $total_estoque += $deposito->deposito->saldo ?>
+                            <tr>
+                                <td><?php echo ($deposito->deposito->nome) ?></td>
+                                <td style="text-align:right"><?php echo ($deposito->deposito->saldo) ?></td>
+                            </tr>        
+                        <?php endforeach; ?>
+                        <tr style="font-weight: bold;">
+                            <td>Total</td>
+                            <td><?php echo $total_estoque ?></td>
+                        </tr>
+                    </table>
+                </td>
+                
+            </tr>
+            <tr>
+                <td colspan="2"><hr></td>
+            </tr>
+        <?php 
+        endforeach;
+        ?>
+        </table>
+        <?php
+        
     }
     public function atualizar_estoque_tiny(){
         global $wpdb;
