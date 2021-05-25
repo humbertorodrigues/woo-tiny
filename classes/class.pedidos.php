0524bf33e30b1845c73d7130f4bc6e551ce9d503
Humberto Rodrigues <humberto.rodrigues@gmail.com>
2021-05-25 14:01:12 -0300

    Libera a visualizaçao dos pedidos para vendedores

--- classes/class.pedidos.php
+++ classes/class.pedidos.php
@@ -4,6 +4,7 @@ class pedidos {
     public function __construct() {
         // add_action( 'woocommerce_order_status_processing', array($this,'enviar_pedido') ,1);
         add_action("woocommerce_order_status_processing", array($this, 'separar_pedidos'),0);
+        add_filter( 'woocommerce_my_account_my_orders_query', [$this,'exibir_pedidos_vendedor'], 10, 1 );
     }
     public function separar_pedidos($order_id) {
         // var_dump($order_id);
@@ -329,6 +330,20 @@ class pedidos {
         $updateNote = 'This order was duplicated from order ' . $original_order_id . '.';
         /* $order->update_status('processing'); */
         $order->add_order_note($updateNote);
+    }
+    
+    public function exibir_pedidos_vendedor( $args ) {
+        
+        $user = wp_get_current_user();
+        
+        if ( in_array( 'vendedores_bw', (array) $user->roles ) ) {
+            
+                unset($args['customer']);            
+                $args['meta_key'] = 'bw_id_vendedor';
+                $args['meta_value'] = get_current_user_id();  
+        }
+        
+        return $args;
     }
 }
 ?>
