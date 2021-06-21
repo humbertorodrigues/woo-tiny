902480214065a8582adec26b9b25a070be989794
Humberto Rodrigues <humberto.rodrigues@gmail.com>
2021-06-21 13:32:16 -0300

    Permite ao admin a edição de canais

--- templates/post-types/shop-order/meta-seller-data.php
+++ templates/post-types/shop-order/meta-seller-data.php
@@ -5,7 +5,14 @@
         <br>
         <strong>Forma de Pagamento: </strong> <?= $payment_method ?>
         <br>
-        <strong>Canal: </strong> <?= $channel ?>
+        <strong>Canal: </strong>
+        <select name="bw_canal_venda" id="">
+            <option value=""></option>
+            <?php foreach ($channels as $key => $ch) {
+                ?> <option <?php echo $ch->ID==$channel_id?"selected":"" ?> value="<?php echo $ch->ID ?>"><?php echo $ch->post_title; ?></option>
+                <?php
+            } ?>
+        </select>
         <br>
     </p>
 
